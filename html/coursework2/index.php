25a681820ae71d2724e27ad218e77a9706940d78
pcnnadi <psxpn2@nottingham.ac.uk>
2023-12-11 12:59:41 +0000

    started with the login page

--- html/coursework2/index.php
+++ html/coursework2/index.php
@@ -1,10 +1,8 @@
-<!DOCTYPE html> <!-- I know it's a php file but it looked weird in my browser without saying this-->
+<!DOCTYPE html> <!-- I know it's a php file but it looked weird in my browser (scroll) without saying this-->
 <html> 
 <head> 
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- to ensure webpage adapts to the width of device screen-->
+	<title>Traffic Incident System: Home</title> <!-- this is what will appear in the browser title bar-->
     <link rel="stylesheet" href="css/format.css"> <!-- linking my css file to format the layout of the home page-->
-    <title>Traffic Incident System: Home</title> <!-- this is what will appear in the browser title bar-->
 </head>
 <body>
     <div class="background-image"></div> <!-- this class will style the background image in my home page -->
@@ -14,7 +12,7 @@
         <h1 class="centre-heading">Traffic Incident System</h1> <!-- heading 1. The main heading on our home page styled to be centred using a class "center-text" -->
 		<p1 class="centre-paragraph">Report on the go! Incident management made easy.</p1> <!-- the only other text on my homepage. a centred paragraph -->
         <img src="images/traffic_light.png" alt="Traffic Light" class="traffic-light"> <!-- traffic light image. specified the source and an alternative text. I learnt that this is good practice for accessibility reasons -->
-		<img src="images/uon.png" alt="Uni Logo" class="uni-logo"> <!-- university of nottingham logo. For this image anx the one above, their respective classes are used for styling-->
+		<img src="images/uon.png" alt="University of Nottingham Logo" class="uon-logo"> <!-- university of nottingham logo. For this image anx the one above, their respective classes are used for styling-->
         
         <div class="button-position"> <!-- a separate division for the login button. This class will handle the position of the login button -->
             <a href="login.php" class="login-button">Login</a> <!-- the actual login button hyperlinked (anchor) to the login page (login.php). text displays login and uses the login-button class for styling -->
